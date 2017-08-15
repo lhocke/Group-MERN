49982ec727e05eba056b7ef7b04901610758d328
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-08-15 13:15:00 -0700

    adds more functionality and index.js

---
+++ app/config/routes.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+var Article = require('../models/Article');
+//render the index.html file when this route is hit
+router.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname, '../public', 'index.html');
+});
+//this will get all the saved articles from the db
+router.get('/api/saved', function(req,res) {
+    Article.findAll({}).exec(function(err, doc) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.json(doc);
+        }
+    });
+});
+router.post('/api/saved', function(req,res) {
+    var savedArticle = new Article(req.body);
+    Article.save(function(err,doc) {
+        if(err) {
+            res.send(err);
+        }
+        else {
+            res.send(doc);
+        }
+    });
+});
+router.delete('/api/saved/:id', function(req,res) {
+    Article.remove(req.params.id, function(err,doc) {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.send(doc);
+        }
+    });
+});
+router.get("*", function(req, res) {
+  res.redirect("/");
+});
+//export these routes so server.js can use them
+module.exports = router;
---
+++ models/Article.js
@@ -0,0 +1,24 @@
+var mongoose = require("mongoose");
+
+var schema = mongoose.Schema;
+
+// create our schema for mongo db
+var articleSchema = new Schema ({
+  title: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  }
+});
+
+var Article = mongoose.model("Article", articleSchema);
+
+// Export the Article model
+module.exports = Article;

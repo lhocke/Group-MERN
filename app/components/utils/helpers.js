e748b73b9fba623bdaca67a9d220ae83edb7c39d
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-08-15 14:20:06 -0700

    organize indentation and comments

--- app/components/utils/helpers.js
+++ app/components/utils/helpers.js
@@ -45,29 +45,3 @@ var helper = {
   }
 
 module.exports = helper
-
-  //   // Figure out the geolocation
-  //   var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key= " + nytAPI + "&q=";
-  //   return axios.get(queryURL).then(function(response) {
-  //     // If get get a result, return that result's formatted address property
-  //     if (response.data.results[0]) {
-  //       return response.data.results[0].formatted;
-  //     }
-  //     // If we don't get any results, return an empty string
-  //     return "";
-  //   });
-  // },
-
-//   // This function hits our own server to retrieve the record of query results
-//   getHistory: function() {
-//     return axios.get("/api");
-//   },
-//
-//   // This function posts new searches to our database.
-//   postHistory: function(location) {
-//     return axios.post("/api", { location: location });
-//   }
-// };
-
-// We export the API helper
-//module.exports = helper;
